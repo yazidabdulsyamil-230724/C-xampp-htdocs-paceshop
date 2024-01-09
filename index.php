67f4a03b6cacf8c411c941a8047c9a161d391103
yazidabdulsyamil-230724 <137190070+yazidabdulsyamil-230724@users.noreply.github.com>
2024-01-09 17:27:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Toko Baju Muslim | Pace</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>Toko Baju Muslim | PACE</p>
+  </div>
+
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>copyright by | pace | Repost by <a href="" target="_blank">Framework Id</a></p>
+  </div>
+
+</body>
+
+</html>
